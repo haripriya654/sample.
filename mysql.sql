3bf3a57164c8bf9cea9d34c6812b6bf495fb7c51
haripriya654 <148970845+haripriya654@users.noreply.github.com>
2023-11-03 10:49:15 +0530

    Add files via upload

---
+++ mysql.sql
@@ -0,0 +1,13 @@
+
+create database userdbset;
+use userdbset;
+create table movies(name varchar(20),description varchar(20),actor varchar(20));
+
+insert into movies values
+("123","ABC","abc"),
+("456","DEF","abc"),
+("789","GHI","abc");
+
+select * from movies;
+
+
